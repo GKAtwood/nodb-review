75381c33cac2f07e71773b70cadcd13d88a5fe62
GKAtwood <gabrielle.k85@gmail.com>
2020-06-25 16:33:25 -0600

    ddfd

--- src/Components/Grass.js
+++ src/Components/Grass.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 
+//Grass is responsible for displaying wildPokemon sent from Finder.js. It also invokes
+//the catch function, creating a req.body and passing it as an argument to catchFn.
 class Grass extends Component {
+    handleCatch = () => {
+        const {pokemon} = this.props;
+        let newPokemon = {
+            name: pokemon.name,
+            img: pokemon.sprites.front_default
+        }
+
+        this.props.catchFn(newPokemon);
+        this.props.refreshFn();
+    }
+
     render(){
         return (
-            <div>Grass</div>
+            <div onClick={this.handleCatch}>
+                <img src={this.props.pokemon.sprites.front_default} alt={this.props.pokemon.name}/>
+                <p>{this.props.pokemon.name}</p>
+            </div>
         )
     }
 }
--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Pokemon from './Pokemon';
 
+//Pokedex is used to pass caughtPokemon to Pokemon.js. It does this through mapping the pokemon
+//array received from props and mapping over it, passing each item into the Pokemon component.
 const Pokedex = props => {
+    const mappedPokemon = props.caughtPokemon.map((pokemon, i) => (
+        <Pokemon 
+            key={i}
+            pokemon={pokemon}
+            nameFn={props.nameFn}
+            releaseFn={props.releaseFn}/>
+    ))
+
     return (
-        <div>Pokedex</div>
+        <div>
+            <h1>Pokedex</h1>
+            <div className='poke-flex'>
+                {mappedPokemon}
+            </div>
+        </div>
     )
 }
 
